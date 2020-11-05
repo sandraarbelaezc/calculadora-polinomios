772e087544c4c28f8220170a4240c69e6a670138
sandraarbelaezc <sandraarbelaez@gmail.com>
2020-11-04 21:48:15 -0500

    Para esta versión, se agrega la función del menú principal

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from calculadora import calc
+
+calculo = calc()
+
+def menu():
+    print("*******       CALCULADORA       *******")
+    print("*  Selecciona una opción:             *")
+    print("*  1. Suma                            *")
+    print("*  2. Resta                           *")
+    print("*  3. Multipliación entre polinomios  *")
+    print("*  4. Multiplicación por un escalar   *")
+    print("*  5. Evaluación del polinomio        *")
+    print("*  0. Salir                           *")
+    print("***************************************")
+    
+    opcion = input("Opción: ")
+
+    try:
+        if int(opcion) >= 0 and int(opcion) <= 5:
+            return(int(opcion))
+        else:
+            print("Opción incorrecta, por favor ingrese una opción entre 1 y 5")
+            return(int(input("Opción: ")))
+    except:
+        print("Opción incorrecta, por favor ingrese una opción entre 1 y 5")
+        return(int(input("Opción: ")))
