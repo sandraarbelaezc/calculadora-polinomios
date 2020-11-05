2b9c445b4f9dbc7a9c97b8e893c67eb83e93cec7
sandraarbelaezc <sandraarbelaez@gmail.com>
2020-11-04 21:40:22 -0500

    Para esta versión, se agrega el método de suma de dos polinomios

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from numpy.polynomial import polynomial as P
+
+class calc:
+
+    def suma(self, x, y):
+        '''argumentos de entrada: dos arrays de 1D, cada uno con un polinomio
+        retorna: la suma de los dos polinomios
+        '''
+        sum = P.polyadd(x,y)
+        print("--------------------------------------------------------------")
+        print("El resultado de sumar ", str(x), "+", str(y), " es: ", str(sum))
+        print("--------------------------------------------------------------")
