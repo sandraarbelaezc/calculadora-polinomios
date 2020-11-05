bde7a25c02853749cc50a8eb4851351e5782bfe7
sandraarbelaezc <sandraarbelaez@gmail.com>
2020-11-04 21:51:32 -0500

    Para esta versión, se agrega el ciclo infinito y la lectura y transformación de los polinomios a un array de numpy

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ def menu():
     except:
         print("Opción incorrecta, por favor ingrese una opción entre 1 y 5")
         return(int(input("Opción: ")))
+
+while(True):
+    opcion = menu()
+
+    if opcion == 0:
+        print("Fin")
+        break
+
+    else:
+        
+        num11 = int(input("Ingrese el número de elementos del primer coeficiente: ")) 
+        num22 = int(input("Ingresa el número de elementos del segundo coeficiente: "))
+
+        num1 = list(map(int,input("\nIngrese los elementos del primer coeficiente: ").strip().split(',')))[:num11]
+        num2 = list(map(int,input("\nIngrese los elementos del segundo coeficiente: ").strip().split(',')))[:num22]
+
+        num1 = np.array(num1)
+        num2 = np.array(num2)
